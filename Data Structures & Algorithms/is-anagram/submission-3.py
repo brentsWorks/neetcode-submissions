cbdeef22d96ae579f94017c839b10d11730af083
Brent <134455660+brentsWorks@users.noreply.github.com>
2026-06-21 12:32:05 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Store character counts in respective hash maps
+        # compare each map at the end
+        # O(n + m) time col -> n is len(s), m is len(t)
+
+
+        if len(s) != len(t): return False
+
+        countS, countT = {}, {}
+
+        # s and t must be same length so we can just pick one to pass thru
+        for i in range(len(s)):
+            # initialize count to 0 if needed, and add 1 to count
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
